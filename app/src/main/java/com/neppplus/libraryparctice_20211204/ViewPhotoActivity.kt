8f31999d585e066404c0ee90c235117c63b6e27e
yeonjin seong <syj3405@naver.com>
2021-12-04 15:40:05 +0900

    사진 크게보기 화면 추가

---
+++ app/src/main/java/com/neppplus/libraryparctice_20211204/ViewPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.libraryparctice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+    }
+}
