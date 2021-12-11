9b4e78a7d10e1ae58efc68ec792aa4b05cf4e319
yeonjin seong <syj3405@naver.com>
2021-12-11 11:44:41 +0900

    메인화면으로 2초 지나면 이동

--- app/src/main/java/com/neppplus/libraryparctice_20211204/SplashActivity.kt
+++ app/src/main/java/com/neppplus/libraryparctice_20211204/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.neppplus.libraryparctice_20211204
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,14 +14,26 @@ class SplashActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setupEvents{
+    fun setupEvents(){
 
 
     }
 
-    fun setValues{
+    fun setValues() {
+
+//2초가 지나면 => 메인화면으로 이동
+
+        val myHandler = Handler( Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
 
 
 
+        },2000)
     }
 }
