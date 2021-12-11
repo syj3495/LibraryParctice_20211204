5d8523beaeed09568fdf269d28cc9ce26590fe5f
yeonjin seong <syj3405@naver.com>
2021-12-11 11:23:05 +0900

    앱 시작화면 -. SplashActivity로 변경

---
+++ app/src/main/java/com/neppplus/libraryparctice_20211204/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.neppplus.libraryparctice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents{
+
+
+    }
+
+    fun setValues{
+
+
+
+    }
+}
